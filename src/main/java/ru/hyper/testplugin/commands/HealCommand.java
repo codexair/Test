a25c9c6e732329b88e3c9ecce12b6fe71e2365e3
m4rsi <71939433+m4rsi@users.noreply.github.com>
2021-04-08 20:18:08 +0300

    Update HealCommand.java

--- src/main/java/ru/hyper/testplugin/commands/HealCommand.java
+++ src/main/java/ru/hyper/testplugin/commands/HealCommand.java
@@ -3,13 +3,25 @@ package ru.hyper.testplugin.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class HealCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(args.length == 0){
+           if(!(sender instanceof Player)){
+            sender.sendMessage("Console is not allowed to use this command!");
             return true;
-        }
+           }
+          if(args.length == 0){
+           Player p = (Player) sender;
+           if(!p.hasPermission("heal.plug"){
+             player.sendMessage("Permission error!");
+             return true;
+          } else {
+           p.setHealth(20);
+           p.sendMessage("You have been healed");
+           return true;
+         }
         return true;
     }
 }
